c93590c5bfa9e4043eacf8f28faa3982dc108187
runedev-games <danielchang171717@gmail.com>
2026-07-10 18:13:56 -0700

    Create characters.js

---
+++ data/characters.js
@@ -0,0 +1,97 @@
+const characters = [
+
+{
+
+    name: "Dorothea",
+
+    house: "Black Eagles",
+
+    birthday: "9/29",
+
+    crest: "--",
+
+    baseClass: "Commoner",
+
+    ability: ["Songstress", "Sword Prowess Lv 1", "Reason Lv 1"],
+  
+    // mhp, str, mag, dex, spd, lck, def, res, cha
+    stats: {
+      hp: 24, 
+      str: 5, 
+      mag: 11, 
+      dex: 6, 
+      spd: 7, 
+      lck: 6, 
+      def: 4, 
+      res: 7, 
+      cha: 8
+    },
+  
+    growths: {
+      hp: 40, 
+      str: 20, 
+      mag: 40, 
+      dex: 45, 
+      spd: 40, 
+      lck: 35, 
+      def: 15, 
+      res: 35, 
+      cha: 40
+    },
+
+    inventory: ["Vulnerary", "Training Sword", "Golden Apple"],
+
+    combat_arts: ["--"],
+
+    skill_levels: {
+      sword: {
+        rank: "E+",
+        strength: "up"
+      },
+      lance: {
+        rank: "E",
+        strength: "--"
+      },
+      axe: {
+        rank: "E",
+        strength: "--"
+      },
+      bow: {
+        rank: "E",
+        strength: "--"
+      },
+      fist: {
+        rank: "E",
+        strength: "--"
+      },
+      reason: {
+        rank: "D",
+        strength: "up"
+      },
+      faith: {
+        rank: "E",
+        strength: "down",
+        budding: true,
+      },
+      authority: {
+        rank: "E",
+        strength: "--"
+      },
+      armor: {
+        rank: "E",
+        strength: "--"
+      },
+      riding: {
+        rank: "E",
+        strength: "down"
+      },
+      flying: {
+        rank: "E",
+        strength: "down"
+      },
+    }
+      
+
+}
+
+];
